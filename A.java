8646bcae2a4a2d55ed430c0e724517fa1a723d2d
swapnil <swapnilit9@gmail.com>
2025-07-08 07:14:48 +0530

    new change

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A{
+   private String name;
+
+
+   public void doSomething(){
+    System.out.println(" I am learning the Github process");
+    System.out.println("thisis new change");
+
+   }
+}
